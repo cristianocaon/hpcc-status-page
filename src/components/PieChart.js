3d9cce9a71a74f0bb92a1db7b8abae397666c41c
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-17 11:35:32 -0500

    add: props to PieChart

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,36 +1,13 @@
 import { Pie } from 'react-chartjs-2';
 
-const state = {
-  labels: ['PD', 'R'],
-  datasets: [
-    {
-      label: 'Status',
-      backgroundColor: ['#63b0cd', '#39393a', '#c1b098', '#e9d2f4', '#9b9b93'],
-      hoverBackgroundColor: [
-        '#63b0cd',
-        '#39393a',
-        '#c1b098',
-        '#e9d2f4',
-        '#9b9b93'
-      ],
-      data: [65, 59]
-    }
-  ]
-}
-
-export default function PieChart() {
+export default function PieChart(props) {
   return (
     <div style={{ position: 'block' }}>
       <Pie
-        data={state}
+        data={props.data}
         height={250}
         width={500}
         options={{
-          title: {
-            display: true,
-            text: 'Jobs Statuses',
-            fontSize: 20,
-          },
           legend: {
             display: true,
           },
