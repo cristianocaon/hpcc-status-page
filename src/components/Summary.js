3207fea8c5d52a0a3ee75e9a24e5477cf825b530
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-16 19:53:27 -0500

    add: styles and overview

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,9 +1,22 @@
+import Filter from './Filter'
 import MainChart from './MainChart';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}));
 
 export default function Summary() {
+  const classes = useStyles();
+
   return (
     <div>
       <div>
+        <h1 className={classes.root}>Overview</h1>
+        <Filter />
         <MainChart />
       </div>
     </div>
