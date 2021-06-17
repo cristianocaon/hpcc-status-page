e2962190dd6205a3f85db237b7d58e4faf518a19
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-17 11:35:17 -0500

    add: props to PieChart

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -3,6 +3,25 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import PieChart from './PieChart';
 
+// Jobs Status 
+const state = {
+  labels: ['PD', 'R'],
+  datasets: [
+    {
+      label: 'Status',
+      backgroundColor: ['#63b0cd', '#39393a', '#c1b098', '#e9d2f4', '#9b9b93'],
+      hoverBackgroundColor: [
+        '#63b0cd',
+        '#39393a',
+        '#c1b098',
+        '#e9d2f4',
+        '#9b9b93'
+      ],
+      data: [65, 59]
+    }
+  ]
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -34,15 +53,15 @@ export default function MainChart() {
     <div className={classes.root}>
       <Paper className={classes.chart} elevation={1}>
         <label className={classes.title}>Job Status</label>
-        <PieChart />
+        <PieChart data={state} />
       </Paper>
       <Paper className={classes.chart} elevation={1}>
         <label className={classes.title}>Node Status</label>
-        <PieChart />
+        <PieChart data={state} />
       </Paper>
       <Paper className={classes.chart} elevation={1}>
         <label className={classes.title}>Job Availability</label>
-        <PieChart />
+        <PieChart data={state} />
       </Paper>
     </div>
   );
