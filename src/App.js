56937e9617d47b839215ee1b0d394fb3437946bb
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-17 09:35:34 -0500

    add: tab routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from './components/Header';
 import Summary from './components/Summary';
+import Jobs from './components/Jobs'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Summary />
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact component={Summary} path="/" />
+          <Route exact component={Jobs} path="/jobs" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
