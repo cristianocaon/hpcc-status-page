76b96b81821eac5ed0a8cec50675b173603ed65f
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-17 11:34:50 -0500

    add: Dashboard page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import Header from './components/Header';
-import Summary from './components/Summary';
-import Jobs from './components/Jobs'
-import Nodes from './components/Nodes'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact component={Summary} path="/" />
-          <Route exact component={Jobs} path="/jobs" />
-          <Route exact component={Nodes} path="/nodes" />
-        </Switch>
-      </BrowserRouter>
+      <Dashboard />
     </div>
   );
 };
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,18 @@
+import Header from '../components/Header';
+import Summary from '../components/Summary';
+import Jobs from '../components/Jobs'
+import Nodes from '../components/Nodes'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+export default function Dashboard() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact component={Summary} path="/" />
+        <Route exact component={Jobs} path="/jobs" />
+        <Route exact component={Nodes} path="/nodes" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
