5cb971d127eebdcae7f3affbede04902a81cfe4a
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-16 20:06:44 -0500

    add: overview font and center

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,7 +5,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    fontFamily: 'Roboto'
   }
 }));
 
