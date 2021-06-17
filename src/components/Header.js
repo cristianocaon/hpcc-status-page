13e1a57cdaf51592cce6fc06f0ff8a5b04c839d5
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-17 09:37:21 -0500

    update: fixed routing tab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
 import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '2em',
+    marginTop: '1em',
+    marginBottom: '1em'
+  }
+}));
 
 export default function Header() {
+  const classes = useStyles();
   return (
-    <div className='Header' style={{ marginTop: '1em', marginBottom: '1em' }}>
-      <Paper elevation={2}>
-        <Tabs
-          indicatorColor="primary"
-          textColor="primary"
-          aria-label="disabled tabs example"
-          centered
-        >
-          <Tab label="Summary" />
-          <Tab label="Jobs" />
-          <Tab label="Nodes" />
+    <div className={classes.root}>
+      <Paper elevation={1}>
+        <Tabs indicatorColor="primary" textColor="primary" aria-label="disabled tabs example" centered>
+          <Link to="/">
+            <Tab label="Summary" />
+          </Link>
+          <Link to="/jobs">
+            <Tab label="Jobs" />
+          </Link>
+          <Link to="/nodes">
+            <Tab label="Nodes" />
+          </Link>
         </Tabs>
       </Paper>
     </div>
