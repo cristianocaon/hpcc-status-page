5ffd8ed09ba8ff3da79aae0ae329b78b685b7013
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-18 08:57:15 -0500

    add: get jobs statuses

--- src/util/ParseJobStatus.js
+++ src/util/ParseJobStatus.js
@@ -1,6 +1,8 @@
 const parseJobStatus = (jobs) => {
-  const status = jobs.jobs.forEach((job) => { job.job_state });
-  return status;
+  // Limiting to only 20 jobs due to performance
+
+  // *** Change later ***
+  return jobs.jobs.slice(0, 20).map((job) => { return job.job_state });
 }
 
 export default parseJobStatus;
