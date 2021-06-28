cc740d0e8873de0a18a87ba422df80c01df5d784
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-28 11:16:13 -0500

    fix: table population and display

--- src/components/TableContainer.jsx
+++ src/components/TableContainer.jsx
@@ -1,15 +1,30 @@
 import { nodes } from '../data/nodes';
 import NodeTable from './NodeTable';
 import parseNodeStatus from '../util/parseNodeStatus';
+import { useState } from 'react'
 import Paper from '@material-ui/core/Paper';
+import Pagination from '@material-ui/lab/Pagination';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
-    margin: '2em',
+    alignItems: 'center',
+    margin: '1em',
+    alignContent: 'center',
+    overflow: 'hidden',
+  },
+  tables: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: '1em',
     alignContent: 'center'
+  },
+  pagination: {
+    margin: '20px'
   }
 }));
 
@@ -17,10 +32,45 @@ const TableContainer = () => {
 
   const classes = useStyles();
 
-  const nodeStatus = parseNodeStatus(nodes).map(obj => {
-    const node = obj.node.split("cpu-")[1];
-    return ({ node: node, state: obj.state })
-  });
+  const [page, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const nodeStatus = parseNodeStatus(nodes);
+
+  const splitRack = data => {
+    let isFirst = true;
+    let racks = [];
+    let rack = [];
+    let count = parseInt(data[0].node.split("-")[1]);
+    for (let i = 0; i < data.length; i++) {
+
+      let [type, rackNum, nodeNum] = data[i].node.split("-");
+
+      if (parseInt(rackNum) < count) {
+        racks.push(rack);
+        rack = [];
+        count--;
+        isFirst = true;
+      } else if (!isFirst && parseInt(rackNum) === count) {
+        rack.push(data[i]);
+      }
+
+      if (isFirst) {
+        const check = 60 - parseInt(nodeNum);
+        if (check > 0) {
+          for (let j = 0; j < check; j++) {
+            rack.push({ node: '---------', state: 'other' });
+          }
+        }
+        isFirst = false;
+        rack.push(data[i]);
+      }
+    }
+    racks.push(rack);
+    return racks;
+  }
 
   const sliceArray = data => {
     const rows = [];
@@ -30,25 +80,9 @@ const TableContainer = () => {
     return rows;
   }
 
-  const splitRack = data => {
-    let racks = [];
-    let rack = [];
-    let count = 2;
-    for (let i = 0; i < data.length; i++) {
-      if (parseInt(data[i].node.charAt(0)) === count) {
-        rack.push(data[i]);
-      } else {
-        racks.push(rack);
-        rack = [];
-        count--;
-        rack.push(data[i])
-      }
-    }
-    racks.push(rack);
-    return racks;
-  }
+  console.log(nodeStatus.reverse())
 
-  const racks = splitRack(nodeStatus.reverse());
+  const racks = splitRack(nodeStatus);
 
   const splitRackRows = racks => {
     const rackRows = racks.map(rack => {
@@ -62,9 +96,14 @@ const TableContainer = () => {
 
   return (
     <Paper className={classes.root}>
-      {rackRows.map(rack => {
-        return <NodeTable rack={rack} />
-      })}
+      <div className={classes.tables}>
+        {rackRows.map(rack => {
+          return <NodeTable rack={rack} />
+        })}
+      </div>
+      <div className={classes.pagination}>
+        <Pagination count={10} page={page} onChange={handleChange} variant="outlined" color="primary" />
+      </div>
     </Paper>
   )
 }
