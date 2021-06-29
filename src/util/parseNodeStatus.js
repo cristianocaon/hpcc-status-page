26e434542a7d31bde5f9bd5f9fba6f58e8bd2df9
Cristiano Caon <cristiano.caon@hotmail.com>
2021-06-29 08:14:27 -0500

    change: amount of test data

--- src/util/parseNodeStatus.js
+++ src/util/parseNodeStatus.js
@@ -1,5 +1,5 @@
 const parseNodeStatus = (nodes) => {
-  return nodes.nodes.slice(0, 120).map(obj => {
+  return nodes.nodes.slice(0, 260).map(obj => {
     return { node: obj.hostname, state: obj.state }
   });
 }
