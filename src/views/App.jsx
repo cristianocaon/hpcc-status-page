cf008328d7bc35871723d0eea0cb7b0b2c361428
Cristiano Caon <cristiano.caon@hotmail.com>
2021-07-09 11:39:02 -0500

    feat: :lipstick: add timer boilerplate

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../components/layout/Header';
 import Content from '../components/layout/Content';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  timer: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontFamily: 'Roboto',
+    margin: '10px',
+    color: '#3D6171'
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
     <div className="App">
       <Router>
         <Header />
+        <label className={classes.timer}>Update request for data: {Date.now()}</label>
         <Content />
       </Router>
     </div>
