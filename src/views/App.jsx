aa07ccc269679f610cc2f8acad883c19b12c8e69
Cristiano Caon <cristiano.caon@hotmail.com>
2021-07-12 09:49:54 -0500

    style: add refresh and icon

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,25 +1,41 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../components/layout/Header';
 import Content from '../components/layout/Content';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSync } from '@fortawesome/free-solid-svg-icons';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  timer: {
+  info: {
     display: 'flex',
     justifyContent: 'center',
     fontFamily: 'Roboto',
     margin: '10px',
     color: '#3D6171'
   },
+  icon: {
+    marginLeft: '10px',
+    '&:hover': {
+      cursor: 'pointer',
+    }
+  }
 }));
 
 const App = () => {
   const classes = useStyles();
+
+  const handleRefresh = (event) => {
+    event.preventDefault();
+    window.location.reload();
+  }
+
   return (
     <div className="App">
       <Router>
         <Header />
-        <label className={classes.timer}>Update data request: {Date.now()}</label>
+        <label className={classes.info}>Automatically refreshing every 2 minutes
+          <a onClick={handleRefresh}><FontAwesomeIcon icon={faSync} className={classes.icon} spin /></a>
+        </label>
         <Content />
       </Router>
     </div>
