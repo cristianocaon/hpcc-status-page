8fe299902f0ef31bf8fb81ac83d439c675cc6abf
Cristiano Caon <cristiano.caon@hotmail.com>
2021-07-14 10:26:19 -0500

    add: test request

--- src/service/requestSummary.js
+++ src/service/requestSummary.js
@@ -1,10 +1,12 @@
-import axios from 'axios';
-
 const { charts, error, partitions } = require('../data/summary.json')
 
+const PROTO = 'http://';
+const ADDR = 'scarlet.hpcc.ttu.edu';
+const PORT = '80';
+const URL = PROTO + ADDR + ':' + PORT + '/slurm-web/summary';
+
 const requestSummary = () => {
-  const url = 'http://scarlet.hpcc.ttu.edu/slurm-web/summary';
-  fetch(url)
+  fetch(URL)
     .then(res => res.json())
     .then(data => console.log(data));
   return !error ? { charts, partitions } : { charts, partitions, error };
