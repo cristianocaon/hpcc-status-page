d4dc297d61ebeea242f797c47d1a8e1e4b02c8eb
Cristiano Caon <cristiano.caon@hotmail.com>
2021-07-14 11:08:50 -0500

    change: fetch request

--- src/service/requestSummary.js
+++ src/service/requestSummary.js
@@ -6,7 +6,7 @@ const PORT = '80';
 const URL = PROTO + ADDR + ':' + PORT + '/slurm-web/summary';
 
 const requestSummary = () => {
-  fetch(URL)
+  const res = fetch(URL)
     .then(res => res.json())
     .then(data => {
       console.log("Success!")
@@ -14,7 +14,11 @@ const requestSummary = () => {
       const { charts, error, partitions } = data;
       return !error ? { charts, partitions } : { charts, partitions, error };
     })
-    .catch(err => console.log("Fetch error: " + err.message));
+    .catch(err => {
+      console.log("Fetch error: " + err.message)
+      return {};
+    });
+  return res;
 }
 
 export default requestSummary;
