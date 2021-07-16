77c33f08352696caed3d1c43a2e2bb0e681b55dc
Cristiano Caon <cristiano.e.caon@gmail.com>
2021-07-16 08:29:58 -0500

    feat: implement basic get request

--- src/service/requestJobs.js
+++ src/service/requestJobs.js
@@ -1,8 +1,22 @@
-const { errors, jobs } = require('../data/jobs.json')
+import axios from 'axios'
 
-const requestJobs = () => {
-  return !errors ? { jobs } : { jobs, errors };
+const PROTO = 'http://';
+const ADDR = 'scarlet.hpcc.ttu.edu';
+const PORT = '80';
+const URL = PROTO + ADDR + ':' + PORT + '/slurm-web/jobs';
 
-}
+const requestSummary = async (setData, setLoading, setError) => {
+  try {
+    const { data } = await axios.get(URL)
+    if (!data.errors) {
+      setData(data.jobs);
+    } else {
+      setError(data.errors);
+    }
+    setLoading(false);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
-export default requestJobs;
+export default requestSummary;
--- src/views/Jobs.jsx
+++ src/views/Jobs.jsx
@@ -7,6 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import { partitionItems } from '../views/Summary';
 import requestJobs from '../service/requestJobs';
+import { DatasetController } from 'chart.js';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,14 +38,18 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const jobsInfo = requestJobs();
+// const jobsInfo = requestJobs();
 
 const Jobs = () => {
   const classes = useStyles();
 
+  const [data, setData] = useState()
   const [partition, setPartition] = useState("");
   const [status, setStatus] = useState("");
+  const [error, setError] = useState(null)
   const [loading, setLoading] = useState(true);
+  const [fields, setFields] = useState();
+  const [rows, setRows] = useState();
 
   const handlePartitionSelection = (event) => {
     setPartition(event.target.innerText);
@@ -55,69 +60,97 @@ const Jobs = () => {
   }
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000)
+    requestJobs(setData, setLoading, setError);
+    const interval = setInterval(() => requestJobs(setData, setLoading, setError), 1000 * 60 * 2);
+    return () => clearInterval(interval);
   }, [])
 
-  let fields = undefined;
-  let data = undefined;
-
-  if (!('errors' in jobsInfo)) {
-    let { jobs } = jobsInfo;
-    fields = Object.keys(jobs[0]).filter(job => {
-      return job !== 'nodelist';
-    }).map(column => {
-      let header = column.toUpperCase();
-      let width = column.length < 6 ? 110 : column.length < 7 ? 120 : column.length < 9 ? 140 : 150;
-      if (column === 'jobid') {
-        column = 'id';
-        header = 'JOBID';
-      }
-      return ({
-        field: column,
-        headerName: header,
-        width: width,
+  useEffect(() => {
+    if (data) {
+      let columnInfo = Object.keys(data[0]).filter(key => {
+        return key !== 'nodelist';
+      }).map(column => {
+        let header = column.toUpperCase();
+        let width = column.length < 6 ? 110 : column.length < 7 ? 120 : column.length < 9 ? 140 : 150;
+        if (column === 'jobid') {
+          column = 'id';
+          header = 'JOBID';
+        }
+        return ({
+          field: column,
+          headerName: header,
+          width: width,
+        });
       });
-    });
+      setFields(columnInfo);
 
-    data = jobs.map(obj => {
-      let { jobid, ...data } = obj;
-      data["id"] = jobid;
-      return data;
-    });
+      let rowData = data.map(obj => {
+        let { jobid, ...data } = obj;
+        data["id"] = jobid;
+        return data;
+      });
+      setRows(rowData);
+    }
+  }, [data])
+
+  // let fields = undefined;
+  // let data = undefined;
+
+  if (!error) {
+    if (loading) return <Loading />
+    // let { jobs } = data;
+    // fields = Object.keys(jobs[0]).filter(job => {
+    //   return job !== 'nodelist';
+    // }).map(column => {
+    //   let header = column.toUpperCase();
+    //   let width = column.length < 6 ? 110 : column.length < 7 ? 120 : column.length < 9 ? 140 : 150;
+    //   if (column === 'jobid') {
+    //     column = 'id';
+    //     header = 'JOBID';
+    //   }
+    //   return ({
+    //     field: column,
+    //     headerName: header,
+    //     width: width,
+    //   });
+    // });
+
+    // data = jobs.map(obj => {
+    //   let { jobid, ...data } = obj;
+    //   data["id"] = jobid;
+    //   return data;
+    // });
 
     return (
       <div>
-        {!loading ?
-          <div>
-            <div className={classes.root}>
-              <div className={classes.filterContainer}>
-                <Filter title="Partition"
-                  partitions={partitionItems}
-                  onClick={handlePartitionSelection}
-                />
-                <Filter title="Status"
-                  onClick={handleStatusSelection} />
-              </div>
-              <DataGrid
-                rows={data}
-                columns={fields}
-                pageSize={8}
-                autoHeight={true}
-                disableColumnMenu={true}
-                disableSelectionOnClick={true}
-              />
-            </div>
+        <div className={classes.root}>
+          <div className={classes.filterContainer}>
+            <Filter title="Partition"
+              partitions={partitionItems}
+              onClick={handlePartitionSelection}
+            />
+            <Filter title="Status"
+              onClick={handleStatusSelection} />
           </div>
-          : <Loading />
-        }
+          <DataGrid
+            rows={rows}
+            columns={fields}
+            pageSize={8}
+            autoHeight={true}
+            disableColumnMenu={true}
+            disableSelectionOnClick={true}
+          />
+        </div>
       </div>
     );
   } else {
-    return <Alert severity="error"
-      className={classes.errors}>
-      <AlertTitle>Error</AlertTitle>'
-      {jobsInfo.errors}'
-    </Alert>
+    return (
+      <Alert severity="error"
+        className={classes.errors}>
+        <AlertTitle>Error</AlertTitle>
+        '{error}'
+      </Alert>
+    )
   }
 }
 
