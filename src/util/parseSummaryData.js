ff3b21eb15641cd1c17679c1dc8050a0204b1faa
Cristiano Caon <cristiano.e.caon@gmail.com>
2021-07-23 12:02:45 -0500

    refactor: rename variable

--- src/util/parseSummaryData.js
+++ src/util/parseSummaryData.js
@@ -1,13 +1,13 @@
 import processLabel from './processLabel';
 
-const parseSummaryData = (jobs) => {
-  const keys = Object.keys(jobs);
+const parseSummaryData = (data) => {
+  const keys = Object.keys(data);
   const labels = [];
   const values = [];
   for (let i = 0; i < keys.length; i++) {
     if (keys[i][0] !== '%' && (keys[i] !== 'total_jobs' && keys[i] !== 'total_nodes' && keys[i] !== 'total_cpu')) {
       labels.push(processLabel(keys[i]));
-      values.push(Object.values(jobs)[i]);
+      values.push(Object.values(data)[i]);
     }
   }
   return [labels, values];
