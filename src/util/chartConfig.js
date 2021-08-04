e3fbcd73e2ae9e6ec64635c341f770a5a49cf218
Cristiano Caon <cristiano.e.caon@gmail.com>
2021-08-04 10:39:34 -0500

    style: change order of legend

--- src/util/chartConfig.js
+++ src/util/chartConfig.js
@@ -1,7 +1,7 @@
 const colors = {
   usage: ['#2d6a4f', '#ffd166', '#393E41'],
   job: ['#ffd166', '#2d6a4f', '#9d4edd', '#393E41'],
-  node: ['#2d6a4f', '#a9def9', '#9d4edd', '#7b2cbf', '#ef233c', '#ffd166', '#52b788', '#393E41'],
+  node: ['#2d6a4f', '#52b788', '#a9def9', '#9d4edd', '#7b2cbf', '#ef233c', '#ffd166', '#393E41'],
 }
 
 const chartConfig = (type, labels, values) => {
